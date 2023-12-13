24440766e267be6daf145a74ec1f4800d55fa2ad
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-12-13 12:11:30 +0800

    updating comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import logo from "/rocket.png";
-import "./App.css";
+// Import required packages and Components
+import {
+  Navigate,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
 import LoginSignup from "./Components/LoginSignup";
 import FirebaseForm from "./Components/FirebaseForm";
 import FirebaseDisplay from "./Components/FirebaseDisplay";
+import NavBar from "./Components/Navbar";
+
+// Firebase local and external imports
 import { signOut } from "firebase/auth";
 import {
   onChildAdded,
@@ -11,42 +19,43 @@ import {
   remove,
   onChildRemoved,
 } from "firebase/database";
-import { Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import { database, auth } from "./firebase";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NavBar from "./Components/Navbar";
+// Import required assest
+import logo from "/rocket.png";
+import "./App.css";
 
+// Firebase Key setup
 const DB_STUDENTS_KEY = "students";
 
+// This component holds the lifted state and implements React Router Dom into the Application
 function App() {
-  const studentListRef = ref(database, DB_STUDENTS_KEY);
-
+  // states required for App
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState({});
-
-  // full student list
   const [students, setStudent] = useState([]);
-
-  // Editing
   const [editing, setEditing] = useState(false);
   const [editingData, setEditingData] = useState({});
 
+  // define and create the Firebase RealTimeDatabase list reference
+  const studentListRef = ref(database, DB_STUDENTS_KEY);
+
+  // useEffect to handle the code executed on load
   useEffect(() => {
+    // Update the current state if a new item is added
     onChildAdded(studentListRef, (data) => {
       setStudent((previousData) => [
         ...previousData,
         { key: data.key, val: data.val() },
       ]);
     });
-
+    // Update the current state if an old item is removed
     onChildRemoved(studentListRef, (data) => {
       setStudent((previousData) =>
         previousData.filter((item) => item.key !== data.key)
       );
     });
-
+    // Update the current state if an item has been edited
     onChildChanged(studentListRef, (data) => {
       setStudent((previousData) =>
         previousData.map((item) =>
@@ -56,10 +65,12 @@ function App() {
     });
   }, []);
 
+  // Delete item function from firebase - removes from the real time database
   const deleteItem = (student) => {
     remove(ref(database, `${DB_STUDENTS_KEY}/${student.key}`));
   };
 
+  // signout function for firebase - resets the user and isLoggedIn
   const handleSignOut = () => {
     signOut(auth).then(() => {
       setIsLoggedIn(false);
@@ -67,11 +78,14 @@ function App() {
     });
   };
 
+  // this compoent ensures that the user is logged in before rendering the JSX in the browser, if the user is not logged in they get redirected
   const RequireAuth = ({ children, redirectTo, user }) => {
     const isAuthenticated = user.email ? true : false;
     return isAuthenticated ? children : <Navigate to={redirectTo} />;
   };
 
+  // Code to develop the browserRouter, all children/ element components have access to navigation and history
+  // Pass any required props into the components as needed
   const router = createBrowserRouter([
     {
       path: "/",
@@ -138,6 +152,7 @@ function App() {
     },
   ]);
 
+  // JSX that is returned and displayed in the application
   return (
     <>
       <div>
@@ -146,6 +161,7 @@ function App() {
         </a>
       </div>
       <h1>Module 2</h1>
+      {/* Setting up and using the Router */}
       <RouterProvider router={router} />
     </>
   );
